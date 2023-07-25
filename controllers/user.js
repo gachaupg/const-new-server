e365d122676d8d61f19fae9cba2bd5d7f5abf251
Ebenezer Okello <okelloebenezer403@gmail.com>
2023-07-25 11:29:06 +0300

    hhhh

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,15 @@
 import user from "../models/user.js";
 
 
-export const getUsers = async (req,res)=>{
+export const getUsers = async (req, res) => {
+    const { page } = req.query;
     try {
-        // const tours = await TourModal.find();
-        // res.status(200).json(tours);
-    
-        const tours = await user.find();
-        res.status(200).json(tours);
-      } catch (error) {
-        res.status(404).json({ message: "Something went wrong" });
-      }
-}
+      // const tours = await TourModal.find();
+      // res.status(200).json(tours);
+  
+      const tours = await UserModal.find();
+      res.json(tours);
+    } catch (error) {
+      res.status(404).json({ message: "Something went wrong" });
+    }
+  };
